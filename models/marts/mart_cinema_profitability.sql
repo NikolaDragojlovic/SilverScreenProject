db90d54a124415f427ac4fd451b367b2fef3897e
Dragojlovic Nikola <thenikola007@gmail.com>
2026-01-07 20:09:29 +0000

    create mart model

---
+++ models/marts/mart_cinema_profitability.sql
@@ -0,0 +1,14 @@
+SELECT  m.movie_id,
+        m.movie_title,
+        m.genre,
+        m.studio,
+        m.month,
+        m.location_id,
+        m.rental_cost,
+        l.ticket_amount AS tickets_sold,
+        l.revenue
+  FROM {{ ref('int_movie_costs') }} AS m
+  LEFT JOIN {{ ref('int_union_locations_revenue') }} AS l
+    ON m.movie_id = l.movie_id
+    AND m.location_id = l.location_id
+    AND m.month = l.transaction_month
